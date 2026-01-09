0a79d272bed5bf9c53b24aa4db3984599b8c645e
lywwind <lywwind@gmail.com>
2026-01-09 10:10:52 +0900

    test

--- examples/kit-nextjs-skate-park/src/lib/sitecore-client.ts
+++ examples/kit-nextjs-skate-park/src/lib/sitecore-client.ts
@@ -1,8 +1,21 @@
-import { SitecoreClient } from '@sitecore-content-sdk/nextjs/client';
+import { SitePathService } from '@sitecore-content-sdk/nextjs';
+import { createGraphQLClientFactory, SitecoreClient } from '@sitecore-content-sdk/nextjs/client';
 import scConfig from 'sitecore.config';
 
 const client = new SitecoreClient({
   ...scConfig,
+  custom: {
+    sitePathService: new SitePathService({
+      clientFactory: createGraphQLClientFactory({
+        api: scConfig.api,
+      }),
+      excludedPaths: getExcludePaths(), // ここで更に二つの選択肢があります。後述
+    }),
+  },
 });
+
+function getExcludePaths(): string[] | undefined {
+  return ['/About', '/NewPageWithImage'];
+}
 
 export default client;
