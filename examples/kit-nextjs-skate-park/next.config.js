4a2bc58f6aa79f7aec52b0a56abfaa7d83c0c7e9
lywwind <lywwind@gmail.com>
2026-02-21 21:35:00 +0900

    test

--- examples/kit-nextjs-skate-park/next.config.js
+++ examples/kit-nextjs-skate-park/next.config.js
@@ -86,7 +86,7 @@ const nextConfig = {
         headers: [
           {
             key: 'Cache-Control',
-            value: 'public, s-maxage=3600, stale-while-revalidate=60',
+            value: 'public, max-age=0, s-maxage=3600, stale-while-revalidate=60',
           },
         ],
       },
