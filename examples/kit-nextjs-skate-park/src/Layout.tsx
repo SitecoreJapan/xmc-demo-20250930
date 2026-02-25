7e94b73547c3df1b7c937bdd2cc3c5a58176476a
lywwind <lywwind@gmail.com>
2026-02-25 15:50:08 +0900

    test

--- examples/kit-nextjs-skate-park/src/Layout.tsx
+++ examples/kit-nextjs-skate-park/src/Layout.tsx
@@ -6,7 +6,7 @@ import Head from 'next/head';
 import { Placeholder, Field, DesignLibrary, Page, LinkField } from '@sitecore-content-sdk/nextjs';
 import Scripts from 'src/Scripts';
 import SitecoreStyles from 'src/components/content-sdk/SitecoreStyles';
-import Script from 'next/script';
+// import Script from 'next/script';
 
 interface LayoutProps {
   page: Page;
