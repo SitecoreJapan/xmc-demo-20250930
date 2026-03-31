26023a93cee754b01ff882874f733ae2daddbefb
lywwind <lywwind@gmail.com>
2026-03-31 19:09:33 +0900

    test

--- examples/kit-nextjs-skate-park/src/Layout.tsx
+++ examples/kit-nextjs-skate-park/src/Layout.tsx
@@ -45,10 +45,10 @@ const Layout = ({ page }: LayoutProps): JSX.Element => {
       }
     }
 
-  //   if (isValidRedirectUrl(redirectTargetUrl)) {
-  //     window.location.replace(redirectTargetUrl);
-  //   }
-  // }, [redirectTargetUrl]);
+    if (isValidRedirectUrl(redirectTargetUrl)) {
+      window.location.replace(redirectTargetUrl.replace('en-001', 'en-global')); // en-global 対応
+    }
+  }, [redirectTargetUrl]);
 
   return (
     <>
