61f93832c9140b013cd92fd909a73ec375a7bb7b
lywwind <lywwind@gmail.com>
2026-05-28 12:09:11 +0900

    test

--- examples/basic-nextjs/next.config.js
+++ examples/basic-nextjs/next.config.js
@@ -48,11 +48,11 @@ const nextConfig = {
         source: '/healthz',
         destination: '/api/healthz',
       },
-      {
-        source: '/-/media/:path*',
-        destination:
-          'https://xmc-sitecoresaa6daf-xmcdemo20253162-test6f9c.sitecorecloud.io/-/media/:path*',
-      },
+      // {
+      //   source: '/-/media/:path*',
+      //   destination:
+      //     'https://xmc-sitecoresaa6daf-xmcdemo20253162-test6f9c.sitecorecloud.io/-/media/:path*',
+      // },
       // robots route
       {
         source: '/robots.txt',
