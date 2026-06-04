6a4b73cb8832d05964944362976bf395d68bc630
lywwind <lywwind@gmail.com>
2026-06-04 14:54:25 +0900

    test

--- examples/kit-nextjs-skate-park/src/pages/[[...path]].tsx
+++ examples/kit-nextjs-skate-park/src/pages/[[...path]].tsx
@@ -116,7 +116,7 @@ export const getStaticProps: GetStaticProps = async (context) => {
     // Next.js will attempt to re-generate the page:
     // - When a request comes in
     // - At most once every 5 seconds
-    revalidate: 5, // In seconds
+    revalidate: 30, // In seconds
     notFound: !page,
   };
 };
