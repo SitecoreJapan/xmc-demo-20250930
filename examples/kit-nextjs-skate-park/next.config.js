f2615702d25d11066184d82f081c8a84b930e77a
lywwind <lywwind@gmail.com>
2026-06-08 14:31:17 +0900

    test

--- examples/kit-nextjs-skate-park/next.config.js
+++ examples/kit-nextjs-skate-park/next.config.js
@@ -7,7 +7,7 @@ console.log('in next.config.js: ', typeValue);
  * @type {import('next').NextConfig}
  */
 const nextConfig = {
-  trailingSlash: true,
+  //trailingSlash: true,
   // Allow specifying a distinct distDir when concurrently running app in a container
   distDir: process.env.NEXTJS_DIST_DIR || '.next',
 
