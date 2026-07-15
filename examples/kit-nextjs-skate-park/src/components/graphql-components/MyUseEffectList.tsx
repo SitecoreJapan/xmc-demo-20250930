c96fe5394c88c3595a1017341f55539f9de48b31
lywwind <lywwind@gmail.com>
2026-07-15 10:49:06 +0900

    test

--- examples/kit-nextjs-skate-park/src/components/graphql-components/MyUseEffectList.tsx
+++ examples/kit-nextjs-skate-park/src/components/graphql-components/MyUseEffectList.tsx
@@ -1,6 +1,5 @@
-import { JSX, useEffect, useState } from 'react';
+import { JSX, useState } from 'react';
 import { gql } from 'graphql-request';
-import { GraphQLRequestClient } from '@sitecore-content-sdk/nextjs/client';
 import client from 'lib/sitecore-client';
 import { GetComponentServerProps } from '@sitecore-content-sdk/nextjs';
 
