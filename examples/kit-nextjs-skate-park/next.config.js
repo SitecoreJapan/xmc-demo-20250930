9c88b85df39dcc5891ba61b3ed8baf30d6ba84b5
lywwind <lywwind@gmail.com>
2026-07-27 11:48:51 +0900

    test

--- examples/kit-nextjs-skate-park/next.config.js
+++ examples/kit-nextjs-skate-park/next.config.js
@@ -85,7 +85,7 @@ const nextConfig = {
       },
       {
         source: '/-/media/:path*',
-        destination: '/api/mediaFetch/:path*',
+        destination: '/api/mediaFetch?path=:path*',
       },
       {
         source: '/robots.txt',
